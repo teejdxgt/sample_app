3304e4473a833cc88bc6ba683779c35a5056c7c9
teejdxgt <tj.lindsley@gmail.com>
2011-11-09 23:08:13 -0500

    User signup complete bastard.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ SampleApp::Application.routes.draw do
   resources :users
   
   match '/signup',  :to => 'users#new'
-  
-  match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'

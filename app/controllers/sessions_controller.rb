6ff477abe44fe29bfab19603956d72bcbe1a3033
teejdxgt <tj.lindsley@gmail.com>
2011-11-22 11:44:13 -0500

    Done with user edit/update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:session][:email], params[:session][:password])
     if user.nil?
-      flash.now[:error] = "Invalid email/password combination you stupid motherfucker!"
+      flash.now[:error] = "Invalid email/password combination."
       @title = "Sign in"
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
